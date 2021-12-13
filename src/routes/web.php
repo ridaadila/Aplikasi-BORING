db6b1ded24c5f98d5b45c19b61b65debc62e8c07
kanarekha <kanarekha8@gmail.com>
2021-12-13 12:54:01 +0700

    add login-regist

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,10 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('boring/home');
 })->name('home');
-
-Route::get('/login', function () {
-    return view('boring/login-register');
-})->name('login');
+/*
+start kana
+*/
+Route::get('/dashboard', [CustomAuthController::class, 'dashboard']);
+Route::get('/login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('/custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
+Route::get('/registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('/custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
+Route::get('/signout', [CustomAuthController::class, 'signOut'])->name('signout');
+/*
+end kana
+*/
+// ini punya yang awal
+// Route::get('/login', function () {
+//     return view('boring/login-register');
+// })->name('login');
 
 Route::get('/contact', function () {
     return view('boring/contact');
